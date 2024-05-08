2b1e570b06d67b658d7b59ff902132f80d736599
Alejandra <monicsalejandra@gmail.com>
2024-05-08 13:26:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { board } from './components/board'
+
+
+
+function App() {
+
+  return <board />;
+
+}
+
+export default App
---
+++ src/components/board.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const board = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default board
