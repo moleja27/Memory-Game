d6983e4ab7f7fa988250323a71baeb2440ecd60b
Alejandra <monicsalejandra@gmail.com>
2024-05-09 19:55:59 +0200

    The first function was created that shuffles images so that they are in different places

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
 
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
 export const Board = () => {
     const [cards, setCards] = useState([]);
     const [flippedCards, setFlippedCards] = useState([]);
